5d34bb1ed4d1e6c895d9f578129654c86aa9c3d7
lazqii <gilangrizqi2016@gmail.com>
2024-02-14 11:53:05 +0700

    tambah style tipis tipis

---
+++ resources/views/about.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h2>GILANG RIZQI RAMADHAN</h2>
+    <img src="img/profil.jpg" alt="Gilang Rizqi Ramadhan" width="200px">
+</body>
+</html>
